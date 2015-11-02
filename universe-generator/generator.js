437b1d961c6e28d7d652f30cf786c676b9333beb
Sébastien Van Eyck <sebastien@ifeelgoods.com>
2015-11-02 15:15:40 +0100

    Started generator + stars configuration

---
+++ universe-generator/generator.js
@@ -0,0 +1,9 @@
+
+var generateSolarSystem = function(size) {
+  var grid = 100;
+  var celestialsPlanets = 10;
+  var sunCelestialSize = 3;
+  var celestialsPlanetsSizeMax = 0.10*sunCelestialSize;
+  var celestialsPlanetsSizeMin = 0.02*sunCelestialSize;
+  var planetSpacingMin = 8;
+};
